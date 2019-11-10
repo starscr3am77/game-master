d01eeded6f8a9d3b5d0bd5d4873ea5dc50ff3005
Shaun Nieves <shaun@unityescaperooms.com>
2019-11-10 14:35:38 -0800

    Timer update

--- distress/timer.py
+++ distress/timer.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-timer = 60
+timer = 3600
 timer_override = 0
 failure_played = 0
 success_played = 0
@@ -19,20 +19,21 @@ while True:
     lever = GPIO.input(17)
     #print(state)
     #print(lever)
+    print(timer)
     sleep(1)
     if state == 0 and timer_override == 0:
         timer = timer - 1
     
     #print (timer)
         
-    if lever == 0 and success_played == 1:
-        timer = 60
+    if lever == 1 and success_played == 1:
+        timer = 3600
         timer_override = 0
         failure_played = 0
         success_played = 0
         
     if failure_played == 1:
-        timer = 60
+        timer = 3600
 
     if timer == 0 and failure_played == 0:
         os.system("killall omxplayer.bin")
@@ -41,7 +42,7 @@ while True:
         os.system("xrefresh -display :0")
         failure_played = 1
         
-    if (lever == 0):
+    if (lever == 1):
         success_played = 0
     else:
         if (success_played == 0):
@@ -51,4 +52,44 @@ while True:
             os.system("sudo -u www-data python /var/www/html/stop_timer.py")
             success_played = 1
             timer_override = 1
+            
+    if (timer == 3540):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 3300):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Warp.mp3 &')
+        
+    if (timer == 3120):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 2820):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 2520):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 2220):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 1920):
+        os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/Incoming.mp3 &')
+        
+    if (timer == 1800):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/30_minutes.mp3 &')
+        
+    if (timer == 900):
+        #os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/15_minutes.mp3 &')
+        
+    if (timer == 300):
+        os.system('pkill -9 mpg123')
+        os.system('mpg123 -q /var/www/html/snd/5_minutes.mp3 &')
             
